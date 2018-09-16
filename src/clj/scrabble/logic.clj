b2b9948a037eb165b367501a5abb7b2deefbd110
Andrea Crotti <andrea.crotti.0@gmail.com>
2018-09-16 18:19:33 +0100

    remove deps

--- src/clj/scrabble/logic.clj
+++ src/clj/scrabble/logic.clj
@@ -7,34 +7,6 @@
 
 (def words-list ["one" "two" "neo"])
 
-(u/flatten-bindings)
-
-(l/run 2 [q]
-  (l/== q 2))
-
-;; permuteo seems to hang forever even with a small list when using run*
-
-
-;; use fresh to create new names
-
-;; we can not multiply x since for example it's a Lvar
-
-(l/run 2 [q]
-  (l/fresh [x y]
-    (l/== [x 2] [1 y])
-    (l/== q [x y])))
-
-(defn my-appendo [l1 l2 o]
-  (l/conde
-   ((l/== l1 ()) (l/== l2 o))
-   ((l/fresh [a d r]
-      (l/conso a d l1)
-      (l/conso a r o)
-      (my-appendo d l2 r)))))
-
-(l/run 2 [q]
-  (my-appendo [1] q [1 2 3 5]))
-
 ;; 1. generate all anagrams of a word
 ;; 2. generate all anagrams given a word with some Holes and various possibilities
 ;;  Given for example H _ l l _
@@ -72,13 +44,5 @@
       #_(l/everyg #(l/membero % letters) vars)
       (init vars hints))))
 
-(solver "h_ll_" "hello")
-
-;; would be nice to use unification directly but it doesn't work
-(u/unify ['?first "Argento" "Baudo"]
-         ["Dario" '?last '?very-last])
-
-(u/subst '[(?a * ?x | 2) + (?b * ?x) + ?c]
-         '{?c 3, ?b 4, ?x 5
-           ?z (?a * 5 | 2)})
-
+(comment
+  (solver "h_ll_" "hello"))
